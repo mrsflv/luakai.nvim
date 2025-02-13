33f882a4ec80f1f9b5d7619d67075edb942e973d
mrsflv <fmorselli@jmawireless.com>
2025-02-13 14:48:44 +0100

    Corrections to fzf-lua.

--- lua/luakai/groups/integrations/fzf.lua
+++ lua/luakai/groups/integrations/fzf.lua
@@ -2,7 +2,7 @@ local M = {}
 
 function M.get()
   return {
-    FzfLuaNormal = { link = "NormalFloat" }, -- Respect fzf-lua's default float bg
+    -- FzfLuaNormal = { link = "NormalFloat" }, -- Respect fzf-lua's default float bg
     FzfLuaBorder = { link = "FloatBorder" },
     FzfLuaTitle = { link = "FloatTitle" },
     FzfLuaHeaderBind = { fg = C.yellow },
@@ -11,7 +11,7 @@ function M.get()
     FzfLuaFzfMatch = { fg = C.blue },
     FzfLuaFzfPrompt = { fg = C.orange },
     FzfLuaPathColNr = { fg = C.gray },
-    FzfLuaPathLineNr = { fg = C.gray },
+    FzfLuaPathLineNr = { fg = C.yellow },
     FzfLuaBufName = { fg = C.fg },
     FzfLuaBufNr = { fg = C.blue },
     FzfLuaBufFlagCur = { fg = C.red },
